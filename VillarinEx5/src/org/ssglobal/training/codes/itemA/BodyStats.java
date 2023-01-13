eda6320a435795f9e21f794f4dbdc21524c365f3
simonvillarin <simon.villarin@gmail.com>
2023-01-13 10:43:02 +0800

    eight commit

--- VillarinEx5/src/org/ssglobal/training/codes/itemA/BodyStats.java
+++ VillarinEx5/src/org/ssglobal/training/codes/itemA/BodyStats.java
@@ -1,6 +1,11 @@
 package org.ssglobal.training.codes.itemA;
 
+import java.io.IOException;
+
 public class BodyStats {
+	private char gender;
+	private int height;
+	private int weight;
 	private final int IDEAL_WEIGHT_FOR_FEMALE = 100;
 	private final int IDEAL_WEIGHT_FOR_MALE = 106;
 	private final int MIN_HEIGHT = 50;
@@ -9,8 +14,10 @@ public class BodyStats {
 	private final double RANGE = 0.15;
 
 	public void inputHeight(char gender, int height) {
-		int weight = computeWeight(gender, height);
-		boolean isOkay = normalWeight(gender, weight);
+		this.gender = gender;
+		this.height = height;
+		int weight = computeWeight();
+		boolean isOkay = normalWeight();
 		
 		System.out.format("Actual Weight: %d \n", weight);
 		if (isOkay) {
@@ -20,8 +27,7 @@ public class BodyStats {
 		}
 	}
 
-	public int computeWeight(char gender, int height) {
-		int weight = 0;
+	public int computeWeight() {
 		int diffInHeight = (height - MIN_HEIGHT);
 		
 		if (diffInHeight > 9) {
@@ -38,7 +44,7 @@ public class BodyStats {
 		return weight;
 	}
 	
-	public boolean normalWeight(char gender, int weight) {
+	public boolean normalWeight() {
 		double minRangeFemale = (IDEAL_WEIGHT_FOR_FEMALE - (IDEAL_WEIGHT_FOR_FEMALE * RANGE));
 		double maxRangeFemale = (IDEAL_WEIGHT_FOR_FEMALE + (IDEAL_WEIGHT_FOR_FEMALE * RANGE));
 		double minRangeMale = (IDEAL_WEIGHT_FOR_MALE - (IDEAL_WEIGHT_FOR_MALE * RANGE));
@@ -51,5 +57,24 @@ public class BodyStats {
 			return true;
 		}
 		return false;
+	}
+	
+	public void print() {
+		byte[] kbheight = new byte[1024];
+		char gender = 0;
+		int height = 0;
+		
+		try {
+			System.out.print("Enter gender ([F] for Female and [M] for Male): ");
+			gender = (char) System.in.read();			
+			System.in.skipNBytes(System.in.available());			
+			System.out.print("Enter height: ");
+			System.in.read(kbheight);
+			String height1 = new String(kbheight).toString().trim();
+			height = Integer.parseInt(height1);
+			inputHeight(gender, height);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- VillarinEx5/src/org/ssglobal/training/codes/itemA/TestBodyStats.java
+++ VillarinEx5/src/org/ssglobal/training/codes/itemA/TestBodyStats.java
@@ -1,30 +1,10 @@
 package org.ssglobal.training.codes.itemA;
 
-import java.io.IOException;
-
 public class TestBodyStats {
 
 	public static void main(String[] args) {
-		byte[] kbheight = new byte[1024];
-		char gender = 0;
-		int height = 0;
-		
-		try {
-			System.out.print("Enter gender ([F] for Female and [M] for Male): ");
-			gender = (char) System.in.read();
-			
-			System.in.skipNBytes(System.in.available());
-			
-			System.out.print("Enter height: ");
-			System.in.read(kbheight);
-			String height1 = new String(kbheight).toString().trim();
-			height = Integer.parseInt(height1);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
 		BodyStats bs = new BodyStats();
-		bs.inputHeight(gender, height);
+		bs.print();
 	}
 
 }
